29ed0b05a4af29060062a66a0a7bf6c76cf38766
bgarnier <bgarnier@aneo.fr>
2018-03-01 13:36:00 +0100

    Make test for unknown users

--- src/OC/UserBundle/DataFixtures/ORM/LoadUser.php
+++ src/OC/UserBundle/DataFixtures/ORM/LoadUser.php
@@ -11,30 +11,66 @@ namespace OC\UserBundle\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use OC\UserBundle\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class LoadUser implements FixtureInterface
+class LoadUser implements FixtureInterface, ContainerAwareInterface
 {
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     public function load(ObjectManager $manager)
     {
+
+        // Get our userManager, you must implement `ContainerAwareInterface`
+        $userManager = $this->container->get('fos_user.user_manager');
+
         // Les noms d'utilisateurs à créer
         $listNames = array('Alexandre', 'Marine', 'Anna');
 
         foreach ($listNames as $name) {
             // On crée l'utilisateur
-            $user = new User;
+            $user =$userManager->createUser();
 
             // Le nom d'utilisateur et le mot de passe sont identiques pour l'instant
             $user->setUsername($name);
-            $user->setPassword($name);
+            $user->setPlainPassword($name);
 
             // On ne se sert pas du sel pour l'instant
-            $user->setSalt('');
+            $user->setEmail($name.'@test.fr');
             // On définit uniquement le role ROLE_USER qui est le role de base
             $user->setRoles(array('ROLE_USER'));
+            $user->setEnabled(True);
 
             // On le persiste
+            $userManager->updateUser($user, true);
             $manager->persist($user);
         }
+
+        // On crée l'utilisateur
+        $user =$userManager->createUser();
+
+        // Le nom d'utilisateur et le mot de passe sont identiques pour l'instant
+        $user->setUsername('bgarnier' );
+        $user->setPlainPassword('bgarnier');
+        $user->setEmail('bgarnier@test.fr');
+        // On ne se sert pas du sel pour l'instant
+        // On définit uniquement le role ROLE_USER qui est le role de base
+        $user->setRoles(array('ROLE_AUTEUR'));
+        $user->setEnabled(True);
+
+        // On le persiste
+        $manager->persist($user);
 
         // On déclenche l'enregistrement
         $manager->flush();
--- tests/AppBundle/Controller/AdvertControllerTest.php
+++ tests/AppBundle/Controller/AdvertControllerTest.php
@@ -14,4 +14,17 @@ class AdvertControllerTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
+
+    # utilisateur non identifié
+    public function testIndexUnknow()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertSame(1, $crawler->filter('html:contains("Ma plateforme d\'annonces")')->count());
+        $this->assertSame(0, $crawler->filter('a:contains("Déconnexion")')->count());
+        $this->assertSame(1, $crawler->filter('a:contains("Connexion")')->count());
+    }
 }
